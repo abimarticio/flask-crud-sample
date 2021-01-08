1e835c8a30616333124fa997092f50128827cdbe
Abigail Marticio <marticiobgl@gmail.com>
2021-01-08 23:00:06 +0800

    chore: reconstruct content

--- api.py
+++ api.py
@@ -1,27 +1,62 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
 
-todos = {}
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
 
-class TodoSimple(Resource):
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
     def get(self, todo_id):
-        # return {todo_id: todos[todo_id]}
-        return {todo_id: todos.get(todo_id)}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
 
     def put(self, todo_id):
-        todos[todo_id] = request.form["data"]
-        return {todo_id: todos.get(todo_id)}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
-api.add_resource(TodoSimple, "/<string:todo_id>")
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 
-if __name__ == "__main__":
-    # access api by
-    # localhost:5000
-    # e.g. curl http://localhost:5000
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
